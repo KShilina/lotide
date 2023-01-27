41432673dd72b963683238046f030096acfbd71f
Ekaterina Shilina <yatakshilina@gmail.com>
2023-01-26 23:04:18 -0500

    Added initial file

---
+++ assertEqual.js
@@ -0,0 +1,29 @@
+/*
+const sum = function(a, b) { 
+  return a + b;
+}
+const sumBuggy = function(a, b) { 
+  return a * b;
+}
+
+// TEST CODE
+console.assert(sumBuggy(1, 2) === 3); // fails, because bug!
+// TEST CODE
+// console.assert(sum(1, 2) === 3);
+// console.assert(sum(1, 20) === 3);
+*/
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+  //`` makes string dynamic parameters and ${} dynamic values 
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// TEST CODE
+//call the function
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
