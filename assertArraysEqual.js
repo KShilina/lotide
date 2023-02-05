4291d57d0787a7e74b70468e32e320d9fef56b45
Ekaterina Shilina <yatakshilina@gmail.com>
2023-02-05 18:15:50 -0500

    Corrected Test code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -23,5 +23,5 @@ const assertArraysEqual = function(actual, expected) {
 
 // TEST CODE
 //call the function
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
