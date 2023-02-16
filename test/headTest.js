2d8f1db3986442e031140f3f8c44fa608a108812
Ekaterina Shilina <yatakshilina@gmail.com>
2023-02-15 23:09:28 -0500

    Set the Lotide project up with Mocha and Chai (M

--- test/headTest.js
+++ test/headTest.js
@@ -1,12 +1,25 @@
-// test/head.js
+const assert = require('chai').assert;
+const head   = require('../head');
 
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// TEST CODE
-//call the function
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5); 
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); 
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+
+});
