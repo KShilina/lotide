20808381ece22c78791a9aa9f059933885e92c7e
Ekaterina Shilina <yatakshilina@gmail.com>
2023-02-16 22:53:13 -0500

    Refactoring by using Mocha & Chai.

--- middle.js
+++ middle.js
@@ -1,14 +1,20 @@
-
-const middle = function(arr) {
-  result  = [];
-  for (let i = 0; i < arr.length; i++) {
-  if (arr[i] === arr[Math.round((arr.length - 1) / 2)]){
-    result.push(arr[i])
-    //console.log(result)
-  }
-    }
-    return result;
+const middle = function (array) {
+  if (array.length < 3) {
+    return [];
   }
 
+  let middleVal = 0;
+
+  //checks if array is even
+  if (array.length % 2 === 0) {
+    middleVal = array.length / 2;//even numbers
+    return array.slice(middleVal - 1, middleVal + 1);
+
+    //Checks if array is odd
+  } else if (array.length % 2 === 1) {
+    middleVal = (array.length - 1) / 2;// odd numbers
+    return array.slice(middleVal, middleVal + 1);
+  }
+};
 
 module.exports = middle;
--- test/headTest.js
+++ test/headTest.js
@@ -23,3 +23,9 @@ describe("#head", () => {
   });
 
 });
+
+//What Is Mocha vs What Is Chai
+
+//Mocha gives us the describe and it functions. Each it is a test, and each test should have at least one assertion.
+
+//Chai is an assertion library. It gives us assertion functions so that we no longer have to use our assertEqual and other assertion functions that we implemented previously. Chai assertion functions are deliberately designed to play nice with testing frameworks like Mocha.
